5047bbc823fd0f1c942f458415b88293567e9d7a
alifehmi <101139947+AliFehmi@users.noreply.github.com>
2025-03-11 00:02:25 -0400

    simple_request

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
+import os
 import streamlit as st
 import fal_client
 import time
 
-# Set your Fal.ai API key
-API_KEY = "0f2711f9-87e3-4d64-aa8b-a6b31aaffeb0:b0ab8c100de1d5ed26b0a0ff7df1512e"
-
-
+# Submit request
 handler = fal_client.submit(
     "fal-ai/flux/dev",
     arguments={
@@ -17,3 +15,4 @@ handler = fal_client.submit(
 )
 
 request_id = handler.request_id
+print(request_id)
